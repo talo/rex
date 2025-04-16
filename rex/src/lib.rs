18b5db5834bcc010fb7c14ccd553d8c1ff846a43
Peter Kelly <peter@pmkelly.net>
2025-04-16 17:37:22 +0700

    chore: Add convenience exports in rex crate

--- rex/src/lib.rs
+++ rex/src/lib.rs
@@ -15,3 +15,22 @@ pub mod parser {
 pub mod type_system {
     pub use rex_type_system::*;
 }
+
+// For convenience
+pub use engine::program::Program;
+pub use json::expr_to_json;
+pub use json::json_to_expr;
+pub use rex_ast::expr::Expr;
+pub use rex_ast::id::Id;
+pub use rex_engine::codec::Decode;
+pub use rex_engine::codec::Encode;
+pub use rex_engine::engine::Builder;
+pub use rex_engine::error::Error;
+pub use rex_engine::eval::Context;
+pub use rex_engine::ftable::Ftable;
+pub use rex_engine::ftable::FtableFn;
+pub use rex_lexer::span::Span;
+pub use rex_type_system::types::ADTVariant;
+pub use rex_type_system::types::ToType;
+pub use rex_type_system::types::Type;
+pub use rex_type_system::types::ADT;
